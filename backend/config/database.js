86154d2c53ae2124675ce7c6abf6a6c8e3e034f7
muhammadalighouri <98492348+muhammadalighouri@users.noreply.github.com>
2023-08-14 17:06:25 +0500

    update

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDatabase = () => {
     mongoose
-        .connect('mongodb+srv://admin:flipcart$7@flipcart.6irf4r8.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+        .connect('mongodb+srv://gali76682:V9Szta0rQYoG8q1o@cluster0.k4bojvw.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             console.log("Mongoose Connected");
         });
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.route('/products').get(getAllProducts);
 router.route('/products/all').get(getProducts);
 
-router.route('/admin/products').get(isAuthenticatedUser, authorizeRoles("admin"), getAdminProducts);
+router.route('/admin/prods').get(isAuthenticatedUser, authorizeRoles("admin"), getAdminProducts);
 router.route('/admin/product/new').post(createProduct);
 
 router.route('/admin/product/:id')
